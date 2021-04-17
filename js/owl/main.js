ce8a01b9b488f9339af2ed64f0e4e049a0689336
unknown <gs.coelho@outlook.com>
2021-04-17 19:15:24 -0300

    cabeçalho e começo da página

---
+++ js/owl/main.js
@@ -0,0 +1,15 @@
+$('.owl-carousel').owlCarousel({
+    
+    loop:true,
+    margin:20,
+    nav:false,
+    responsive:{
+        0:{
+            items:2
+        },
+        400:{
+            items:2
+        },
+       
+    }
+});
