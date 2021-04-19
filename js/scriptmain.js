2502a2e909943f6c68ae6d64e5020f3675f8f9e0
unknown <gs.coelho@outlook.com>
2021-04-19 18:48:57 -0300

    mudança no carrossel inserção de novos textos

---
+++ js/scriptmain.js
@@ -0,0 +1,20 @@
+//Descrição dos personagens na página principal
+function info1(elemento){
+    elemento.innerHTML = "<p>Jogada no top, lutadora e spliter</p>";
+}
+function info2(elemento){
+    elemento.innerHTML = "<p>Jungler, ótimo assassino</p>";
+}
+
+function info3(elemento){
+    elemento.innerHTML = "<p>Mid, alto dano em área</p>";
+}
+
+function info4(elemento){
+    elemento.innerHTML = "<p>Hyper Carry, grande mobilidade e dano</p>";
+}
+
+function info5(elemento){
+    elemento.innerHTML = "<p>Sup/Mid dano explosivo</p>";
+}
+
